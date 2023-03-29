de91796616f3afd2347d11861745a276381dc1c4
PhoonThetPine <thetpine254@gmail.com>
2023-03-29 19:18:14 +0800

    Initial Commit

---
+++ Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Phoon Thet Pine on 29/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
