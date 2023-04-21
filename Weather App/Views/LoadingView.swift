be1b4e0ae363de9e6b5585471f45ed788da1bdb2
PhoonThetPine <thetpine254@gmail.com>
2023-04-21 19:50:45 +0800

    v1

---
+++ Weather App/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  Weather App
+//
+//  Created by Phoon Thet Pine on 29/3/23.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
